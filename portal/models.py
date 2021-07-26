da8535a9142cf1b765f39f9c0e4b061e7a69fa42
rafael <rafael.matthes@live.com>
2021-07-26 00:20:09 -0300

    fix: models

--- portal/models.py
+++ portal/models.py
@@ -11,11 +11,11 @@ class Author(models.Model):
 class Article(models.Model):
     category = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    tittle = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     summary = models.CharField(max_length=255)
     firstParagraph = models.CharField(max_length=255)
     body = models.TextField(blank=True)
 
     def __str__(self):
-        return self.tittle
+        return self.title
 
