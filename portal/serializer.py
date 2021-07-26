aef54fbe9ea8ad500ccdb3d56f1437812001ca79
rafael <rafael.matthes@live.com>
2021-07-26 00:17:36 -0300

    fix: articles serializer

--- portal/serializer.py
+++ portal/serializer.py
@@ -3,16 +3,22 @@ from portal.models import *
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Author
         fields = "__all__"
 
 class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    author_id = serializers.SlugRelatedField(queryset=Author.objects.all(), slug_field='name', write_only=True)
+
     class Meta:
         model = Article
         fields = "__all__"
 
 class getArticlesByCategorySerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    author_id = serializers.SlugRelatedField(queryset=Author.objects.all(), slug_field='name', write_only=True)
 
     class Meta:
         model = Article
