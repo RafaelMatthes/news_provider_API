3493b42fe365e7b46fbc088d668bc32248ef672a
rafael <rafael.matthes@live.com>
2021-07-28 02:04:14 -0300

    feat: model testCase

--- portal/test/test_models.py
+++ portal/test/test_models.py
@@ -12,10 +12,28 @@ class ModelBase(TestCase):
 
         self.article = Article.objects.create(
             category='Lorem Ipsum',
-            author=self.author.id,
-            tittle='Lorem Ipsum massun',
+            author=self.author,
+            title='Lorem Ipsum mussun',
             summary='Lorem Ipsum',
             firstParagraph='Lorem Ipsum',
             body='Lorem Ipsum',
         )
 
+class AuthorModelTest(ModelBase):
+
+    def test_model_creation(self):
+        """ model was created ? """
+
+        self.assertEquals(self.author.name,'Lorem Ipsum')    
+        self.assertEquals(self.author.picture, '')    
+
+    def test_article_creation(self):
+        """ model was created ? """
+
+        self.assertEquals(self.article.category,'Lorem Ipsum')    
+        self.assertEquals(self.article.author, self.author)   
+        self.assertEquals(self.article.title,'Lorem Ipsum mussun')  
+        self.assertEquals(self.article.summary,'Lorem Ipsum')  
+        self.assertEquals(self.article.firstParagraph,'Lorem Ipsum')  
+        self.assertEquals(self.article.body,'Lorem Ipsum')  
+        self.assertEquals(self.article.slug_category,'lorem-ipsum')  
